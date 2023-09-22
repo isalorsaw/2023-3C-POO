1e5d0d0fe26725912ba62380c02f0bbec6beb3dc
isalorsaw <isalorsaw@hotmail.com>
2023-09-21 19:58:17 -0600

    Desarrollo Ejercicio1 21/9

--- Introduccion/PrimerEjercicio.java
+++ Introduccion/PrimerEjercicio.java
@@ -1,8 +1,7 @@
 /*
- * Primer Programa en Java
- * Pedir dos enteros y mostrar la suma
+ * Tarea Semana 1: Ejercicio 1: 
+ * Programa que sume tres números introducidos por el usuario.
 */
-//Elaborado por Ing Isaias Salinas
 //Importacion de Librerias
 import javax.swing.JOptionPane;
 public class PrimerEjercicio
@@ -12,9 +11,27 @@ public class PrimerEjercicio
         //Declaracion de Variables
         int n1=0;
         int n2=0;
+        int n3=0;
+        int suma=0;
         
+        //Input=Entrada
+        //I/O=Input-Output
         String entrada=JOptionPane.showInputDialog("Ingrese el Primer Entero");
         n1=Integer.parseInt(entrada);
         
+        entrada=JOptionPane.showInputDialog("Ingrese el Segundo Entero");
+        n2=Integer.parseInt(entrada);
+        
+        entrada=JOptionPane.showInputDialog("Ingrese el Tercer Entero");
+        n3=Integer.parseInt(entrada);
+        
+        //Proceso
+        suma=n1+n2+n3;
+        
+        //Salida
+        JOptionPane.showMessageDialog(null,"La suma es: "+suma);
+        //40+50+30=120
+        //String result=n1+"+"+n2+"+"+n3+"="+suma;
+        //JOptionPane.showMessageDialog(null,result);
     }
 }
